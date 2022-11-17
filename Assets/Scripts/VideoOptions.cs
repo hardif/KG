1ad2d31149b7c1bce8884b9e34842d8f92ab82e2
hardif <46674066+hardif@users.noreply.github.com>
2022-11-17 18:34:17 +0900

    title
    setting

--- Assets/Scripts/VideoOptions.cs
+++ Assets/Scripts/VideoOptions.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class VideoOptions : MonoBehaviour
 {
+    List<Resolution> resolutions = new List<Resolution>();
+    public TMP_Dropdown resolutionDropdown;
+    public TMP_Text text;
     // Start is called before the first frame update
     void Start()
     {
-        
+           
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        InitUI();   
+    }
+    void InitUI()
+    {
+       // options = this.GetComponent<TMP_Dropdown>();
+        resolutionDropdown.ClearOptions();
+        foreach (Resolution item in resolutions)
+        {
+            TMP_Dropdown.OptionData options = new TMP_Dropdown.OptionData();
+            options.text = item.width + "x" + item.height + "" + item.refreshRate + "hz";
+            //resolutionDropdown.AddOptions(options);
+            //resolutionDropdown.opt  
+            resolutionDropdown.RefreshShownValue();
+
+        }    
     }
 }
