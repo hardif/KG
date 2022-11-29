574e69c98276c2ff043a8281b62c2ed39bec6289
hardif <highkick0117@naver.com>
2022-11-29 22:54:18 +0900

    Ingame UI
    임시구현

--- Assets/Scripts/EscUI.cs
+++ Assets/Scripts/EscUI.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class EscUI : MonoBehaviour
 {
+    public GameObject canvas;
+    int gamestop = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +15,24 @@ public class EscUI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            if (gamestop == 0)
+            {
+                canvas.SetActive(true);
+                gamestop++;
+                Time.timeScale = 0;
+            }
+            else
+            {
+                canvas.SetActive(false);
+                gamestop--;
+                Time.timeScale = 1;
+            }
+            
+        }
         
+
     }
 
     public void OnClickSave()
