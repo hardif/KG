273621ee82914d9b0ae1ea4b4090b1acb6c8079e
hardif <highkick0117@naver.com>
2022-12-05 19:29:24 +0900

    UI
    근처에 있을 시 활성화 UI 구현

--- Assets/Scripts/POPUPController.cs
+++ Assets/Scripts/POPUPController.cs
@@ -4,32 +4,69 @@ using UnityEngine;
 
 public class POPUPController : MonoBehaviour
 {
-    public GameObject popup;
+    public GameObject[] poparr;
+    public GameObject player;
+    public GameObject VGpopup;
+    public GameObject ADSNpopup;
+    public GameObject JSBpopup;
+    Transform playerTrs;
+    float[,] PlayerAndGhostDist;
+    int ghostnum;
     private bool lockenter;
     // Start is called before the first frame update
     void Start()
     {
+        poparr = new GameObject[3];
+        string ThisGhost;
+        ThisGhost = this.gameObject.name;
+        if (ThisGhost == "JangSanBum")
+        {
+            ghostnum = 0;
+        }
+        if (ThisGhost == "VirginGhost")
+        {
+            ghostnum = 1;
+        }
+        if (ThisGhost == "Auduksini")
+        {
+            ghostnum = 2;
+        }
+        poparr[0] = JSBpopup;
+        poparr[1] = VGpopup;
+        poparr[2] = ADSNpopup;
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerTrs = player.GetComponent<Transform>();
+        PlayerAndGhostDist = new float[3, 1];
+
         lockenter = true;
-        popup.SetActive(true);
-        Time.timeScale = 0;
-        Cursor.lockState = CursorLockMode.Locked;
+        //popup.SetActive(true);
         //Cursor.visible = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyUp(KeyCode.Return) && lockenter)
+        PlayerAndGhostDist[ghostnum, 0] = Mathf.Round(Mathf.Abs(Vector3.Distance(playerTrs.position, this.transform.position)));
+        if(PlayerAndGhostDist[ghostnum,0] < 10 && lockenter)
         {
-            onClickPopUp();
+            onClickPopUp(ghostnum);
+
+        }
+        if (Input.GetKeyUp(KeyCode.Return) && !lockenter)
+        {
+            poparr[ghostnum].SetActive(false);
+            Time.timeScale = 1;
         }
     }
-    public void onClickPopUp()
+    public void onClickPopUp(int ghostnum)
     {
+        
+        poparr[ghostnum].SetActive(true);
+        Time.timeScale = 0;
         lockenter = false;
-        Time.timeScale = 1;
-        popup.SetActive(false);
-        Cursor.lockState = CursorLockMode.None;
+        //Cursor.lockState = CursorLockMode.None;
         //Cursor.visible = false;
+
+        
     }
 }
